71c5c7117da2819ddeed78d0e458b6e25b1dd11e
kinny94 <ad.sam007@gmail.com>
2017-11-10 11:53:38 -0500

    Added steps

--- Up and Running/Top Down Services/javafirstws/src/main/java/com/trainings/ws/soap/PaymentProcessor.java
+++ Up and Running/Top Down Services/javafirstws/src/main/java/com/trainings/ws/soap/PaymentProcessor.java
@@ -1,9 +1,15 @@
 package com.trainings.ws.soap;
 
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
 import com.trainings.ws.soap.dto.PaymentProcessorRequest;
 import com.trainings.ws.soap.dto.PaymentProcessorResponse;
 
+@WebService(name="PaymentProcessor")
 public interface PaymentProcessor {
 
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest);
+	public @WebResult(name="response") PaymentProcessorResponse processPayment(
+			@WebParam(name="paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest);
 }
