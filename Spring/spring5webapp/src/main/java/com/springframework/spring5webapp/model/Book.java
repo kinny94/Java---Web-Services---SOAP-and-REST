4fddd90644e81b5e74a90d21e90bd490e9aae70c
kinny94 <ad.sam007@gmail.com>
2018-01-29 19:51:04 -0500

    Added first spring project

---
+++ Spring/spring5webapp/src/main/java/com/springframework/spring5webapp/model/Book.java
@@ -0,0 +1,82 @@
+package com.springframework.spring5webapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Book {
+
+    private String title;
+    private String isbn;
+    private String publisher;
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(){
+
+    }
+
+    public Book( String title, String isbn, String publisher ){
+
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+
+    }
+
+    public Book( String title, String isbn, String publisher, Set<Author> authors ){
+
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+
+    }
+
+    private String getTitle(){
+
+        return title;
+
+    }
+
+    public void setTitle( String title ){
+
+        this.title = title;
+
+    }
+
+    private String getIsbn(){
+
+        return isbn;
+
+    }
+
+    public void setIsbn( String isbn ){
+
+        this.isbn = isbn;
+
+    }
+
+    private String getPublisher(){
+
+        return publisher;
+
+    }
+
+    public void setPublisher( String publisher ){
+
+        this.publisher = publisher  ;
+
+    }
+
+    public Set<Author> getAuthors() {
+
+        return authors;
+
+    }
+
+    public void setAuthors( Set<Author> authors ){
+
+        this.authors = authors;
+
+    }
+
+}
