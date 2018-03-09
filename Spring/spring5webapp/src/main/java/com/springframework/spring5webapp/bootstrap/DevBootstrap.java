40c0c4a495301960bfea6a2b1671c88b81d764d9
kinny94 <ad.sam007@gmail.com>
2018-03-08 22:36:00 -0500

    Added data to the database

---
+++ Spring/spring5webapp/src/main/java/com/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,45 @@
+package com.springframework.spring5webapp.bootstrap;
+
+import com.springframework.spring5webapp.model.Author;
+import com.springframework.spring5webapp.model.Book;
+import com.springframework.spring5webapp.repositories.AuthorRepository;
+import com.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
+
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository ) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    private void initData(){
+        //Eric
+        Author eric = new Author( "Eric", "Evans" );
+        Book  ddd = new Book( "Domain Driven Design", "1234", "Harper Collins" );
+        eric.getBooks().add( ddd );
+        ddd.getAuthors().add( eric );
+
+        authorRepository.save( eric );
+        bookRepository.save( ddd );
+
+        // Chris
+        Author chris = new Author( "Chris", "Evans" );
+        Book  ddd2 = new Book( "Hey What's up", "1241", "Chris" );
+        chris.getBooks().add( ddd2 );
+
+        authorRepository.save( chris );
+        bookRepository.save( ddd2 );
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        initData();
+    }
+}
