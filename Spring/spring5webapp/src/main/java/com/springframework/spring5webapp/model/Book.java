dbbdfa2abd159a5422b9c7998a486b7679ddb7bc
kinny94 <ad.sam007@gmail.com>
2018-03-08 22:20:16 -0500

    Added crud repositories

--- Spring/spring5webapp/src/main/java/com/springframework/spring5webapp/model/Book.java
+++ Spring/spring5webapp/src/main/java/com/springframework/spring5webapp/model/Book.java
@@ -2,6 +2,7 @@ package com.springframework.spring5webapp.model;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -56,7 +57,18 @@ public class Book {
         return isbn;
     }
 
-    public void setIsbn( String isbn ){
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    public void setIsbn(String isbn ){
         this.isbn = isbn;
     }
 
@@ -76,4 +88,17 @@ public class Book {
         this.authors = authors;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
 }
