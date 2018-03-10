18921e43b9bbbee02d852b6597fbe79bb2e2d469
kinny94 <ad.sam007@gmail.com>
2018-03-10 16:28:20 -0500

    Added notes

--- Spring/dependency-injection-demo/src/main/java/com/example/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
+++ Spring/dependency-injection-demo/src/main/java/com/example/dependencyinjectiondemo/DependencyInjectionDemoApplication.java
@@ -1,12 +1,18 @@
 package com.example.dependencyinjectiondemo;
 
+import com.example.dependencyinjectiondemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(DependencyInjectionDemoApplication.class, args);
+		MyController controller = ( MyController ) context.getBean( "myController" );
+		controller.hello();
+
 	}
 }
